e77bace9a83ba5236573bb76c24a9d8b68721878
Can yilmaz <github-sayitcanyilmaz65@gmail.com>
2025-12-18 18:42:29 +0300

    feat: Product sınıfına displayInfo metodu eklendi.

--- src/envanter/Product.java
+++ src/envanter/Product.java
@@ -1,36 +1,30 @@
 package envanter;
 
-public class Product {
+public class Product implements Storable {
     private String name;
     private int quantity;
     private double price;
 
-    public void setName(String name) {
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    Product(String name, int quantity, double price) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setQuantity(int quantity) {
         this.quantity = quantity;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setPrice(double price) {
         this.price = price;
     }
+    @Override
+    public void displayInfo(){
+        System.out.println("---ÜRÜN BİLGİLERİ---");
+        System.out.println("İsim: "+name);
+        System.out.println("Stok adedi: "+quantity);
+        System.out.println("Fiyat: "+price+" TL");
+    }
 
-    public double getPrice() {
-        return price;
-    }
-    Product(String name , int quantity , double price){
-        this.name=name;
-        this.quantity=quantity;
-        this.price=price;
-    }
 }
