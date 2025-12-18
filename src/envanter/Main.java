c98a2c741c5d838ef91fed5d690819600fcd94b2
Can yilmaz <github-sayitcanyilmaz65@gmail.com>
2025-12-18 18:45:06 +0300

    feat: Main sınıfında örnek nesne oluşturuldu ve test edildi.

--- src/envanter/Main.java
+++ src/envanter/Main.java
@@ -1,6 +1,13 @@
 package envanter;
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        Product p1=new Product("Gofret",100,20);
+        Product p2=new Product("Bisküvi",100,25);
+        Product p3=new Product("Kek",100,15);
+        p1.displayInfo();
+        p2.displayInfo();
+        p3.displayInfo();
+
 
     }
 }
