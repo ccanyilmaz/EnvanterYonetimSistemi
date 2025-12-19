59a634787b32ffa52a485b11e7b5ff25c2a71a83
Can yilmaz <github-sayitcanyilmaz65@gmail.com>
2025-12-19 20:04:49 +0300

    Product sinifina metotlar eklendi.

--- src/envanter/Main.java
+++ src/envanter/Main.java
@@ -1,12 +1,9 @@
 package envanter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Product p1=new Product("Gofret",100,20);
-        Product p2=new Product("Bisküvi",100,25);
-        Product p3=new Product("Kek",100,15);
-        p1.displayInfo();
-        p2.displayInfo();
-        p3.displayInfo();
+
 
 
     }
--- src/envanter/Product.java
+++ src/envanter/Product.java
@@ -2,29 +2,58 @@ package envanter;
 
 public class Product implements Storable {
     private String name;
-    private int quantity;
+    private int stock;
     private double price;
+    private Supplier supplier;
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
-
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-
-    Product(String name, int quantity, double price) {
+    public Product(String name, int stock, double price,Supplier supplier) {
         this.name = name;
-        this.quantity = quantity;
+        this.stock =stock;
         this.price = price;
+        this.supplier=supplier;
+    }
+
+    @Override
+    public void stokArtir(int quantity) {
+        if (quantity>0) {
+            this.stock += quantity;
+            System.out.println(name+" stoğuna "+quantity+" eklendi. Yeni stok: "+stock);
+        }
     }
     @Override
-    public void displayInfo(){
-        System.out.println("---ÜRÜN BİLGİLERİ---");
-        System.out.println("İsim: "+name);
-        System.out.println("Stok adedi: "+quantity);
-        System.out.println("Fiyat: "+price+" TL");
+    public void stokAzalt(int quantity) {
+        if (quantity > 0 && quantity <= stock) {
+            this.stock -= quantity;
+            System.out.println(name+" stoğundan "+quantity+" düşürüldü. Yeni stok "+stock);
+        }
+        else
+            System.out.println("HATA: Yetersiz stok!");
     }
 
+    public String getName() { return name; }
+    public int getStock() { return stock; }
+    public double getPrice() { return price; }
+    public Supplier getSupplier(){return supplier;}
+
+    public void setName(String name) {this.name = name;}
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            System.out.println("Hata: Fiyat 0'dan küçük olamaz!");
+        }
+    }
+    public void setStock(int stock) {
+        if (stock >= 0) {
+            this.stock = stock;
+        } else {
+            System.out.println("Hata: Stok miktarı negatif bir değerle güncellenemez!");
+        }
+    }
+
+    public void setSupplier(Supplier supplier) {this.supplier = supplier;}
+    @Override
+    public String toString() {
+        return "Ürün:" + name + " | stok: "+stock + " | fiyat: " + price + " | tedarikçi: " + supplier.getName();
+    }
 }
