1c40dcfdd9c48385d040851dbf6cb4f913b9cfdd
Can yilmaz <github-sayitcanyilmaz65@gmail.com>
2025-12-25 23:04:48 +0300

    Inventory sınıfı içeriği dolduruldu.

--- src/envanter/Inventory.java
+++ src/envanter/Inventory.java
@@ -1,9 +1,32 @@
 package envanter;
 import java.util.ArrayList;
 public class Inventory {
-    private ArrayList<Product>products=new ArrayList<>();
-    public void addProduct(Product p){
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p) {
         products.add(p);
         System.out.println(p.getName() + " başarıyla envantere eklendi.");
     }
+
+    public ArrayList<Product> searchProducts(String name) {
+        ArrayList<Product> found = new ArrayList<>();
+        for (Product p : products) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                found.add(p);
+            }
+        }
+        return found;
+    }
+
+    public void displayInventory() {
+        if (products.isEmpty()) {
+            System.out.println("Envanter şu an boş.");
+        } else {
+            System.out.println("\n--- GÜNCEL ENVANTER LİSTESİ ---");
+            for (Product p : products) {
+                System.out.println(p);
+            }
+        }
+    }
 }
+
