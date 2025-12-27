a3d826862ac69a14992547d3944b3d8e398474f9
Can yilmaz <github-sayitcanyilmaz65@gmail.com>
2025-12-27 14:32:57 +0300

    ürün silme metodu eklendi.

--- src/envanter/Inventory.java
+++ src/envanter/Inventory.java
@@ -1,32 +1,36 @@
-package envanter;
-import java.util.ArrayList;
-public class Inventory {
-    private ArrayList<Product> products = new ArrayList<>();
+    package envanter;
+    import java.util.ArrayList;
+    public class Inventory {
+        private ArrayList<Product> products = new ArrayList<>();
 
-    public void addProduct(Product p) {
-        products.add(p);
-        System.out.println(p.getName() + " başarıyla envantere eklendi.");
-    }
-
-    public ArrayList<Product> searchProducts(String name) {
-        ArrayList<Product> found = new ArrayList<>();
-        for (Product p : products) {
-            if (p.getName().equalsIgnoreCase(name)) {
-                found.add(p);
-            }
+        public void addProduct(Product p) {
+            products.add(p);
+            System.out.println(p.getName() + " başarıyla envantere eklendi.");
         }
-        return found;
-    }
+        public void removeProduct(String name) {
+            products.removeIf(p -> p.getName().equalsIgnoreCase(name));
+            System.out.println(name + " envanterden silindi.");
+        }
 
-    public void displayInventory() {
-        if (products.isEmpty()) {
-            System.out.println("Envanter şu an boş.");
-        } else {
-            System.out.println("\n--- GÜNCEL ENVANTER LİSTESİ ---");
+        public ArrayList<Product> searchProducts(String name) {
+            ArrayList<Product> found = new ArrayList<>();
             for (Product p : products) {
-                System.out.println(p);
+                if (p.getName().equalsIgnoreCase(name)) {
+                    found.add(p);
+                }
+            }
+            return found;
+        }
+
+        public void displayInventory() {
+            if (products.isEmpty()) {
+                System.out.println("Envanter şu an boş.");
+            } else {
+                System.out.println("\n--- GÜNCEL ENVANTER LİSTESİ ---");
+                for (Product p : products) {
+                    System.out.println(p);
+                }
             }
         }
     }
-}
 
